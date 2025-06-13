22d15322a775e0c1fa7f9586e55c6f33050cb028
unknown <nishabk910@gmail.com>
2025-06-13 17:49:17 +0530

    updated

--- assessments/week09/week09/cdr.cpp
+++ assessments/week09/week09/cdr.cpp
@@ -8,357 +8,224 @@
 #include<vector>
 #include<map>
 
-
-
 using namespace std;
 mutex mtx;
 
-enum logLevel
-{
-	INFO = 0,
-	DEBUG,
-	WARNING,
-	ERROR
+
+enum logLevel {
+    INFO = 0,
+    DEBUG,
+    WARNING,
+    ERROR
 };
-class Logger
-{
-	ofstream logFile;
+
+
+class Logger {
+    ofstream logFile;
 
 public:
-	Logger(const char* filename)
-	{
-		try
-		{
-			logFile.open(filename, ios::app);
-			if (!logFile.is_open())
-				throw "Log File cannot be opened.";
-		}
-		catch (const char* s)
-		{
-			cout << "Error: " << s << endl;
-			exit(0);
-		}
-	}
+    Logger(const char* filename)
+    {
+        try {
+            logFile.open(filename, ios::app);
+            if (!logFile.is_open())
+                throw "Log File cannot be opened.";
+        }
+        catch (const char* s)
+        {
+            cout << "Error: " << s << endl;
+            exit(0);
+        }
+    }
 
-	const char* levelToString(int l)
-	{
-		switch (l)
-		{
-		default:
-			return "UNKNOWN";
-		case INFO:
-			return "INFO";
-		case DEBUG:
-			return "DEBUG";
-		case WARNING:
-			return "WARNING";
-		case ERROR:
-			return "ERROR";
-		}
-	}
+    const char* levelToString(int l) {
+        switch (l) {
+        default: return "UNKNOWN";
+        case INFO: return "INFO";
+        case DEBUG: return "DEBUG";
+        case WARNING: return "WARNING";
+        case ERROR: return "ERROR";
+        }
+    }
 
-	void log(int level, const char* message)
-	{
-		logFile << "[" << levelToString(level) << "] " << message << endl;
-	}
-	
-
+    void log(int level, const char* message) {
+        lock_guard<mutex> lock(mtx);
+        logFile << "[" << levelToString(level) << "] " << message << endl;
+    }
 };
 
-class user
-{
+
+class User {
 private:
-	map<string, string>users;
-	const string userFile = "calllog.txt";
-	void loadUser()
-	{
-		ifstream in(userFile.c_str());
-		string u, p;
-		while (cin >> u >> p)
-		{
-			users[u] = p;
-		}
-		in.close();
-	}
+    map<string, string> users;
+    const string userFile = "calllog.txt";
 
-	void saveUser(const string& username, const string& password)
-	{
-		ofstream out(userFile.c_str());
-		cout << username << "" << password << endl;
-		out.close();
-	}
+    void loadUsers()
+    {
+        ifstream in(userFile);
+        string u, p;
+        while (in >> u >> p) 
+        {
+            users[u] = p;
+        }
+        in.close();
+    }
+
+    void saveUser(const string& username, const string& password) 
+    {
+        ofstream out(userFile, ios::app);
+        out << username << " " << password << endl;
+        out.close();
+    }
 
 public:
-	user()
-	{
-		loadUser();
-	}
+    User() {
+        loadUsers();
+    }
 
-	bool signUp
-	{
-		string username, password;
+    bool signUp(Logger& logger)
+    {
+        string username, password;
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password: ";
+        cin >> password;
 
-	cout << "enter username:";
-		cin >> username;
-		cout << "enter password:";
-		cin >> password;
+        if (users.find(username) != users.end()) 
+        {
+            logger.log(WARNING, "Username already exists");
+            return false;
+        }
 
-		if (user.find(user) != user.end())
-		{
-			log(WARNING"user enter valid info");
-			return false;
-		}
+        users[username] = password;
+        saveUser(username, password);
+        logger.log(INFO, "User signed up successfully");
+        return true;
+    }
 
-		user[username] = password;
+    bool login(Logger& logger) {
+        string username, password;
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password: ";
+        cin >> password;
 
-		saveUser(username, password);
-		log(INFO, "sign in");
-		return true;
+        if (users.find(username) != users.end() && users[username] == password) {
+            logger.log(INFO, "Login successful");
+            return true;
+        }
 
-		bool login
-		{
-			string username,password;
-		cout << "enter username:";
-		cin >> username;
-		cout << "enter password:";
-		cin >> password;
-		if (user.find(user) != user.end() && user[user] = password)
-		{
-			log(INFO, "login successful");
-			return true;
-		}
-		log(FATAL, "invalid info");
-		return false;
-		}
-	};
+        logger.log(ERROR, "Invalid login attempt");
+        return false;
+    }
+};
 
 
-	class customer
-	{
-	private:
-		string brand;
-
-		int inVoicein;
-		int outVoicein;
-		int inVoiceout;
-		int outVoiceout;
-
-		int insmsin;
-		int outsmsin;
-		int insmsout;
-		int outsmsout;
-
-		int mbDownload;
-		int mbUpload;
-
-
-	public:
-		customer() : inVoicein(0), outVoicein(0), inVoiceout(0), outVoiceout(0)
-			insmsin(0), outsmsin(0), insmsout(0), outsmsout(0) mbDownload (0), mbUpload(0){}
-
-		void setBrand(const string& b) {
-			brand = b;
-		}
-		string getBrand() const {
-			return brand;
-		}
-
-		void addInVoiceIn(int duration) {
-			inVoicein += duration;
-		}
-		void addOutvoiceIn(int duration) {
-			outVoicein += duration;
-		}
-		void addInVoiceOut(int duration) {
-			inVoiceout += duration;
-		}
-		void addOutVoiceOut(int duration) {
-			outVoiceout += duration;
-		}
-		void addInSMSIn(int count = 1)
-		{
-			insmsin += count;
-		}
-		void addOutSMSIn(int count = 1)
-		{
-			outsmsin += count;
-		}
-		void addInSMSOut(int count = 1)
-		{
-			insmsout += count;
-		}
-		void addOutSMSOut(int count = 1)
-		{
-			outsmsout += count;
-		}
-		void addMBdownload(int mb) {
-			mbDownload += mb;
-		}
-		void addMBupload(int mb)
-		{
-			mbUpload += mb;
-		}
-
-		void billinginfo(const string& msisdn) const
-		{
-			cout << "customer id:" << msisdn << "(" << brand << ") \n";
-			cout << "service within mobile operator" << endl;
-			cout << "\t incoming voice call duration" << inVoicein << "\n";
-			cout << "\t outgoing voice call duration" << outVoicein << "\n";
-			cout << "\tincoming sms duration" << insmsin << "\n";
-			cout << "\t outgoing sms duration" << outsmsin << "\n";
-			cout << "service outside mobile operator" << endl;
-			cout << "\t incoming voice call duration" << inVoiceout << "\n";
-			cout << "\t outgoing voice call duration" << outVoiceout << "\n";
-			cout << "\tincoming sms duration" << insmsout << "\n";
-			cout << "\t outgoing sms duration" << outsmsout << "\n";
-			cout << "internet" << endl;
-			cout << "\t mbDownload: " << mbDownload << "|" << "\tmbuploaded" << mbUpload << "\n";
-		}
-	};
-
-	class operator
-	{
-	private:
-		string brand;
-
-		int inVoice;
-		int outVoice;
-		int insms;
-		int outsms;
-		int mbDownload;
-		int mbUpload;
-
-	public:
-		operator() :inVoice(0), outVoice(0), insms(0), outsms(0), mbDownload(0),mbUpload(0) {}
-		void setBrand(const string& b)
-		{
-			brand = b;
-		}
-		string getBrand() const {
-			return brand;
-		}
-		void addInVoice(int duration) {
-			inVoice += duration;
-		}
-		void addOutvoice(int duration)
-		{
-			outVoice += duration;
-		}
-		void addInSMS(int count = 1)
-		{
-			insmsin += count;
-		}
-		void addOutSMS(int count = 1)
-		{
-			outsmsin += count;
-		}
-		void addMBdownload(int mb) {
-			mbDownload += mb;
-		}
-		void addMBupload(int mb)
-		{
-			mbUpload += mb;
-		}
-		void billinginfo(ofstream& out, const string& msisdn) const
-		{
-			cout << "#operator database" << msisdn << "(" << brand << ") \n";
-			cout << "\t incoming voice call duration" << inVoice << "\n";
-			cout << "\t outgoing voice call duration" << outVoice << "\n";
-			cout << "\tincoming sms duration" << insms << "\n";
-			cout << "\t outgoing sms duration" << outsms << "\n";
-			cout << "\t mbDownload: " << mbDownload << "|" << "\tmbuploaded" << mbUpload << "\n"
-
-		}
-	};		
-
-class CDR
-{
+class Customer {
 private:
-	string msisdn;
-	string operatorBrand;
-	string operator_MCCMNC;
-	string callType;
-	int duration;
-	double download;
-	double upload;
-	string thirdparty_MSISSDN;
-	string thirdpartyoperator_MSISSDN;
+    string brand;
+    int inVoicein, outVoicein, inVoiceout, outVoiceout;
+    int insmsin, outsmsin, insmsout, outsmsout;
+    int mbDownload, mbUpload;
+
 public:
+    Customer() : inVoicein(0), outVoicein(0), inVoiceout(0), outVoiceout(0),
+        insmsin(0), outsmsin(0), insmsout(0), outsmsout(0), mbDownload(0), mbUpload(0) {}
 
-CDR (const string& line)
-{
-	stringstream ss(line);
-	getline(ss, msisdn, '|');
-	getline(ss, operatorBrand, '|');
-	getline(ss, operator_MCCMNC, '|');
-	getline(ss, callType, '|');
-	ss >> duration;
-	ss.ignore(1, '|');
-	ss >> upload;
-	ss.ignore(1, '|');
-	getline(ss, thirdparty_MSISSDN);
-	getline(ss, thirdpartyoperator_MSISSDN);
+    void setBrand(const string& b) { brand = b; }
+    string getBrand() const { return brand; }
 
-    //void customerbilling(){}
-	// void interoperator(){}
+    void addInVoiceIn(int duration)
+    { inVoicein += duration; }
+    void addOutVoiceIn(int duration)
+    
+    { outVoicein += duration; }
+    void addInVoiceOut(int duration) 
+    { inVoiceout += duration; }
+    void addOutVoiceOut(int duration)
+    { outVoiceout += duration; }
+    void addInSMSIn(int count = 1) 
+    { insmsin += count; }
+    void addOutSMSIn(int count = 1)
+    { outsmsin += count; }
+    void addInSMSOut(int count = 1) 
+    { insmsout += count; }
+    void addOutSMSOut(int count = 1) 
+    { outsmsout += count; }
+    void addMBdownload(int mb) 
+    { mbDownload += mb; }
+    void addMBupload(int mb) 
+    { mbUpload += mb; }
 
-
+    void billingInfo(const string& msisdn) const {
+        cout << "Customer ID: " << msisdn << " (" << brand << ")\n";
+        cout << "Service within mobile operator:\n";
+        cout << "  Incoming voice call duration: " << inVoicein << "\n";
+        cout << "  Outgoing voice call duration: " << outVoicein << "\n";
+        cout << "  Incoming SMS: " << insmsin << "\n";
+        cout << "  Outgoing SMS: " << outsmsin << "\n";
+        cout << "Service outside mobile operator:\n";
+        cout << "  Incoming voice call duration: " << inVoiceout << "\n";
+        cout << "  Outgoing voice call duration: " << outVoiceout << "\n";
+        cout << "  Incoming SMS: " << insmsout << "\n";
+        cout << "  Outgoing SMS: " << outsmsout << "\n";
+        cout << "Internet:\n";
+        cout << "  MB Downloaded: " << mbDownload << ", MB Uploaded: " << mbUpload << "\n";
+    }
 };
 
 
-/*
-class customer 
-{
-protected:
-	string customerid;
-	vector<int>incomingVoicecall;
-	vector<int>outgoingVoicecall;
-	vector<int>incomingSMS;
-	vector<int>outgoingSMS;
+class CDR {
+private:
+    string msisdn;
+    string operatorBrand;
+    string operator_MCCMNC;
+    string callType;
+    int duration;
+    double download;
+    double upload;
+    string thirdparty_MSISDN;
+    string thirdpartyoperator_MSISDN;
+
 public:
-*/
-
-
+    CDR(const string& line) {
+        stringstream ss(line);
+        getline(ss, msisdn, '|');
+        getline(ss, operatorBrand, '|');
+        getline(ss, operator_MCCMNC, '|');
+        getline(ss, callType, '|');
+        ss >> duration;
+        ss.ignore(1, '|');
+        ss >> download;
+        ss.ignore(1, '|');
+        ss >> upload;
+        ss.ignore(1, '|');
+        getline(ss, thirdparty_MSISDN, '|');
+        getline(ss, thirdpartyoperator_MSISDN);
+    }
 };
-class signup
-{
-protected:
-
-	string uname;
-	string password;
-public:
-	signup(string username, string password):uname(username){}
-	void display()
-	{
-		cout << "welcome" << endl;
-		char choice;
-		cin >> choice;
-
-		switch (choice)
-		{
-
-		default:
-			cout << "invalid input" << endl;
-			break;
-		case1:
-			cout << "sign up" << endl;
-			break;
-		case2:
-			cout << "log in" << endl;
-			break;
-		case3:
-			cout << "exit" << endl;
-			break;
 
 
-		}
-		cout << endl;
+int main() {
+    Logger logger("calllog.txt");
+    User u;
 
-	}
+    int choice;
+    cout << "1. Sign Up\n2. Login\nEnter choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+        u.signUp(logger);
+    else if (choice == 2)
+        u.login(logger);
+    else
+        cout << "Invalid choice\n";
+
+    return 0;
+}
 
 
-	
 
 
-};
+
+
